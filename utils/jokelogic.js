1742087e9175d2136a9a518cf4b29724fb436701
Jammingjames <jamesedgal00@gmail.com>
2026-02-09 20:29:10 +0100

    Converted to ESM and added error handling

--- utils/jokelogic.js
+++ utils/jokelogic.js
@@ -3,12 +3,12 @@
  * @param {Array} jokesArray - The array of joke objects.
  * @returns {Object} - A single random joke object.
  */
-const getRandomJoke = (jokesArray) => {
+export const getRandomJoke = (jokesArray) => {
+    // Check if the array is invalid or empty
     if (!jokesArray || jokesArray.length === 0) {
-        return null; 
+        throw new Error("The jokes array is empty or undefined. Please provide a valid array of jokes.");
     }
+
     const randomIndex = Math.floor(Math.random() * jokesArray.length);
     return jokesArray[randomIndex];
 };
-
-module.exports = { getRandomJoke };
